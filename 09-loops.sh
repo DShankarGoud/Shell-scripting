21cd58d4ae6d01505c69a37b7364d402119721f0
shiva shankar <sivas1475@gmail.com>
2022-11-18 03:24:15 +0530

    roboshop

---
+++ 09-loops.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Two loops , for and while
+# But when to go with which loop
+
+# For loop , If our inputs are alist of values/strings then we go with for loop
+
+for fruit in apple orange banana
+do
+  echo Fruit Name = $fruit
+  sleep 1
+done
