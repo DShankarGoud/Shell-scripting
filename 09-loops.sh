952c852c0841e4c679bf700d01bcbdfdcda24b7b
shiva shankar <sivas1475@gmail.com>
2022-11-18 03:32:18 +0530

    roboshop

--- 09-loops.sh
+++ 09-loops.sh
@@ -13,7 +13,7 @@ done
 
 # While loop, If we want to run the loop number of times based on a expression then we go with while loop
 a=10
-while [ $1 -gt 0 ]; do
+while [ $a -gt 0 ]; do
   echo $a
   a=$(($a-1))
 done
