0bfe675ade5506bb6acc932e7e7b0a766f7db556
shiva shankar <sivas1475@gmail.com>
2022-11-18 03:31:24 +0530

    roboshop

--- 09-loops.sh
+++ 09-loops.sh
@@ -10,3 +10,10 @@ do
   echo Fruit Name = $fruit
   sleep 1
 done
+
+# While loop, If we want to run the loop number of times based on a expression then we go with while loop
+a=10
+while [ $1 -gt 0 ]; do
+  echo $a
+  a=$(($a-1))
+done
